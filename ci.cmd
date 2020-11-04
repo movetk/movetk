273c6a5197089217e361025ef5dcd431edfa1ec6
Custers <b.a.custers@tue.nl>
2020-11-04 22:05:59 +0100

    Trying to fix compiler detection for windows. Temporarily disabled dependencies building, should be enabled when succesful
    Signed-off-by: Custers <b.a.custers@tue.nl>

--- ci.cmd
+++ ci.cmd
@@ -1,8 +1,11 @@
-REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+:: Base folder for visual studio compiler
+SET VC_FOLDER = C:/Program\ Files\ (x86)/Microsoft\ Visual\ Studio/2019/Enterprise/VC
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+SET CL_SUFFIX = Tools/MSVC/14.27.29016/bin/Hostx64/x64/cl.exe
 
 RD /S /Q build
 MKDIR build
 CD build
-cmake -GNinja -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake -DWITH_CGAL_BACKEND=OFF -DCMAKE_CXX_COMPILER=C:/Program\ Files\ (x86)/Microsoft\ Visual\ Studio\ 14.0/VC/bin/cl.exe -DCMAKE_C_COMPILER=C:/Program\ Files\ (x86)/Microsoft\ Visual\ Studio\ 14.0/VC/bin/cl.exe -DBUILD_DOC=ON ..
+cmake -GNinja -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake -DWITH_CGAL_BACKEND=OFF -DCMAKE_CXX_COMPILER=%VC_FOLDER%/%CL_SUFFIX% -DCMAKE_C_COMPILER=%VC_FOLDER%/%CL_SUFFIX% -DBUILD_DOC=ON ..
 cmake --build .
 ctest -VV
