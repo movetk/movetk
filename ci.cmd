3bf5791ed3d92537def5f2d014d4d231a1c53573
Custers <b.a.custers@tue.nl>
2020-11-04 23:42:34 +0100

    Some modifications that may help with finding msvc
    Signed-off-by: Custers <b.a.custers@tue.nl>

--- ci.cmd
+++ ci.cmd
@@ -1,13 +1,15 @@
 :: Base folder for visual studio compiler
 SET VC_FOLDER=C:/Program\ Files\ (x86)/Microsoft\ Visual\ Studio/2019/Enterprise/VC
 :: Setup VC variables on path
-REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
 :: Suffix for compiler executable in the VC_FOLDER
 SET CL_SUFFIX=Tools/MSVC/14.27.29016/bin/Hostx64/x64/cl.exe
-DIR "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC"
+DIR "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.27.29016\bin\Hostx64\x64"
 RD /S /Q build
 MKDIR build
 CD build
+SET CC=cl.exe
+SET CXX=cl.exe
 cmake -GNinja -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake -DWITH_CGAL_BACKEND=OFF -DCMAKE_CXX_COMPILER=%VC_FOLDER%/%CL_SUFFIX% -DCMAKE_C_COMPILER=%VC_FOLDER%/%CL_SUFFIX% -DBUILD_DOC=ON ..
 cmake --build .
 ctest -VV
