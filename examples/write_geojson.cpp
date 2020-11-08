34d8483ae0341f50fc4ebee9635739c751f72ebf
Aniket Mitra <aniket.mitra@here.com>
2020-11-08 15:21:02 +0100

    Add example for writing output in GeoJSON format
    Signed-off-by: Aniket Mitra <aniket.mitra@here.com>

---
+++ examples/write_geojson.cpp
@@ -0,0 +1,259 @@
+/*
+ * Copyright (C) 2018-2020 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+//
+// Created by Mitra, Aniket on 08/11/2020.
+//
+
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include "movetk/io/GeoJSON.h"
+#include "movetk/test_data.h"
+#include "GeometryBackendTraits.h"
+#include "movetk/io/ProbeReader.h"
+#include "movetk/algo/Statistics.h"
+#include "movetk/TrajectoryReader.h"
+#include "movetk/HereTrajectoryTraits.h"
+
+constexpr int LON_Idx = here::c2d::raw::ProbeColumns::LON;
+constexpr int LAT_Idx = here::c2d::raw::ProbeColumns::LAT;
+constexpr int TS_Idx = here::c2d::raw::ProbeColumns::SAMPLE_DATE;
+constexpr int DEVICEID_Idx = here::c2d::raw::ProbeColumns::PROBE_ID;
+constexpr int SPEED_Idx = here::c2d::raw::ProbeColumns::SPEED;
+
+typedef GeometryKernel::MovetkGeometryKernel MovetkGeometryKernel;
+
+struct Distance
+{
+    MovetkGeometryKernel::NT operator()(MovetkGeometryKernel::MovetkPoint p0, MovetkGeometryKernel::MovetkPoint p1)
+    {
+        std::vector<MovetkGeometryKernel::NT> ll0;
+        std::copy(p0.begin(), p0.end(), std::back_inserter(ll0));
+        std::vector<MovetkGeometryKernel::NT> ll1;
+        std::copy(p1.begin(), p1.end(), std::back_inserter(ll1));
+        return distance_exact(ll0[1], ll0[0], ll1[1], ll1[0]);
+    }
+};
+
+template <class Trajectory_t>
+class ComputeStatistics
+{
+private:
+    std::string device_id, trajectory_id;
+    std::size_t num_points;
+    typename MovetkGeometryKernel::NT length, traj_duration;
+    typename MovetkGeometryKernel::NT mean_speed, median_speed, min_speed;
+    typename MovetkGeometryKernel::NT max_speed, var_speed;
+    typename MovetkGeometryKernel::NT time_mode;
+    std::vector<std::pair<std::string, std::string>> trajectory_properties;
+    typedef std::vector<std::pair<std::string, std::string>>::const_iterator iterator;
+
+public:
+    ComputeStatistics(Trajectory_t &trajectory, std::size_t count)
+    {
+        device_id = trajectory.template get<DEVICEID_Idx>()[0];
+        trajectory_properties.push_back(std::make_pair("device_id", device_id));
+        trajectory_id = device_id + "_" + std::to_string(count);
+        trajectory_properties.push_back(std::make_pair("trajectory_id", trajectory_id));
+        num_points = trajectory.size();
+        trajectory_properties.push_back(std::make_pair("num_points", std::to_string(num_points)));
+
+        Distance distanceFunc;
+
+        movetk_algorithms::TrajectoryLength<Trajectory_t, MovetkGeometryKernel,
+                                            Distance, LON_Idx, LAT_Idx>
+            lenCalc(distanceFunc);
+        length = lenCalc(trajectory);
+        trajectory_properties.push_back(std::make_pair("length", std::to_string(length)));
+        movetk_algorithms::TrajectoryDuration<Trajectory_t, TS_Idx> duration;
+        traj_duration = duration(trajectory);
+        trajectory_properties.push_back(std::make_pair("duration", std::to_string(traj_duration)));
+
+        // Show speed statistics:
+        using SpeedStat = movetk_algorithms::TrajectorySpeedStatistic<Trajectory_t, MovetkGeometryKernel, Distance, LON_Idx, LAT_Idx, TS_Idx>;
+        SpeedStat speedStat(distanceFunc);
+        using Stat = typename SpeedStat::Statistic;
+        mean_speed = speedStat(trajectory, Stat::Mean);
+        trajectory_properties.push_back(std::make_pair("mean_speed", std::to_string(mean_speed)));
+        median_speed = speedStat(trajectory, Stat::Median);
+        trajectory_properties.push_back(std::make_pair("median_speed", std::to_string(median_speed)));
+        min_speed = speedStat(trajectory, Stat::Min);
+        trajectory_properties.push_back(std::make_pair("min_speed", std::to_string(min_speed)));
+        max_speed = speedStat(trajectory, Stat::Max);
+        trajectory_properties.push_back(std::make_pair("max_speed", std::to_string(max_speed)));
+        var_speed = speedStat(trajectory, Stat::Variance);
+        trajectory_properties.push_back(std::make_pair("var_speed", std::to_string(var_speed)));
+
+        // Show time mode
+        movetk_algorithms::TrajectoryTimeIntervalMode<Trajectory_t, TS_Idx> timeMode;
+
+        time_mode = timeMode(trajectory);
+        trajectory_properties.push_back(std::make_pair("time_mode", std::to_string(time_mode)));
+    }
+
+    void set_property(std::string &key, std::string &val)
+    {
+        trajectory_properties.push_back(std::make_pair(key, val));
+    }
+
+    iterator
+    begin()
+    {
+        return std::cbegin(trajectory_properties);
+    }
+
+    iterator
+    end()
+    {
+        return std::cend(trajectory_properties);
+    }
+
+    typename MovetkGeometryKernel::NT
+    get_num_points()
+    {
+        return num_points;
+    }
+
+    typename MovetkGeometryKernel::NT get_length()
+    {
+        return length;
+    }
+
+    typename MovetkGeometryKernel::NT get_duration()
+    {
+        return traj_duration;
+    }
+
+    typename MovetkGeometryKernel::NT get_mean_speed()
+    {
+        return mean_speed;
+    }
+
+    typename MovetkGeometryKernel::NT get_median_speed()
+    {
+        return median_speed;
+    }
+
+    typename MovetkGeometryKernel::NT get_min_speed()
+    {
+        return min_speed;
+    }
+
+    typename MovetkGeometryKernel::NT get_max_speed()
+    {
+        return max_speed;
+    }
+
+    typename MovetkGeometryKernel::NT get_var_speed()
+    {
+        return var_speed;
+    }
+
+    typename MovetkGeometryKernel::NT get_time_mode()
+    {
+        return time_mode;
+    }
+};
+
+template <class Trajectory_t, class PropertyIterator>
+void write_geojson(Trajectory_t &trajectory, PropertyIterator first, PropertyIterator beyond,
+                   std::ofstream &ofjson)
+{
+
+    //write GeoJSON
+    GeoJSONGeometry geom;
+    GeoJSONProperties prop;
+    GeoJSONFeature feat;
+
+    auto lats = trajectory.template get<LAT_Idx>();
+    auto lons = trajectory.template get<LON_Idx>();
+    auto ts = trajectory.template get<TS_Idx>();
+
+    std::vector<int> timestamps;
+    std::transform(std::begin(ts), std::end(ts), std::back_inserter(timestamps),
+                   [](auto val) -> int { return static_cast<int>(val.ts()); });
+
+    rapidjson::Document geometry = geom(std::begin(lats), std::end(lats),
+                                        std::begin(lons), std::begin(timestamps));
+
+    rapidjson::Document properties = prop(first, beyond);
+    rapidjson::Document feature = feat(geometry, properties);
+    rapidjson::StringBuffer strbuf;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(strbuf);
+    feature.Accept(writer);
+    ofjson << strbuf.GetString();
+    strbuf.Clear();
+}
+
+int main(int argc, char **argv)
+{
+    std::ios_base::sync_with_stdio(false);
+    init_logging(logging::trivial::trace);
+
+    // Specializations for the Commit2Data raw probe format
+    using TrajectoryTraits = here::c2d::raw::ColumnarTrajectoryTraits;
+    using ProbeTraits = typename TrajectoryTraits::ProbeTraits;
+
+    // Create trajectory reader
+    std::unique_ptr<ProbeReader<ProbeTraits>> probe_reader;
+    if (argc < 2)
+    {
+        // Use built-in test data if a file is not specified
+        probe_reader = ProbeReaderFactory::create_from_string<ProbeTraits>(testdata::c2d_raw_csv);
+    }
+    else
+    {
+        // Example: Process trajectories from a (zipped) CSV file (e.g., probe_data_lametro.20180918.wayne.csv.gz)
+        probe_reader = ProbeReaderFactory::create<ProbeTraits>(argv[1]);
+    }
+    using ProbeInputIterator = decltype(probe_reader->begin());
+    auto tr = TrajectoryReader<TrajectoryTraits, ProbeInputIterator>(probe_reader->begin(), probe_reader->end());
+
+    std::ofstream ofjson("output_trajectories.geojson");
+    ofjson.setf(std::ios::fixed);
+
+    ofjson << "{\"type\":\"FeatureCollection\",\"features\":[";
+
+    std::size_t count = 0;
+    for (auto tit = tr.begin(); tit != tr.end();)
+    {
+
+        auto trajectory = *tit;
+        if (trajectory.size() == 1)
+        {
+            ++tit;
+            continue;
+        }
+
+        ComputeStatistics statistics(trajectory, count);
+        auto property_iterator_first = statistics.begin();
+        auto property_iterator_beyond = statistics.end();
+        write_geojson(trajectory, property_iterator_first, property_iterator_beyond, ofjson);
+        if (++tit == tr.end())
+            continue;
+        else
+            ofjson << ",";
+
+        count++;
+    }
+
+    ofjson << "]}" << std::endl;
+    return 0;
+}
