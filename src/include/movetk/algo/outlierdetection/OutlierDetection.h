81246259f5de8c512c4ee12332604c90b2691522
TUE\bacusters <b.a.custers@tue.nl>
2022-05-03 12:33:39 +0200

    Split outlier detection code into separate headers

---
+++ src/include/movetk/algo/outlierdetection/OutlierDetection.h
@@ -0,0 +1,9 @@
+#ifndef MOVETK_OUTLIERDETECTION_OUTLIERDETECTION_H
+#define MOVETK_OUTLIERDETECTION_OUTLIERDETECTION_H
+namespace movetk::outlierdetection {
+
+// Stub for outlier detection algorithms
+template <typename GeometryKernel, typename Predicate, typename AlgorithmTag>
+class OutlierDetection {};
+}  // namespace movetk::outlierdetection
+#endif
