dd3997173fa105e6c3bdb10d4f8460a93e63255d
TUE\bacusters <b.a.custers@tue.nl>
2022-10-04 13:05:51 +0200

    Create import targets when possibly missing due to old style cmake packages, update dependency resolution accordingly. Ignore vcpkg installation folder

--- cmake/ResolveMovetkDependencies.cmake
+++ cmake/ResolveMovetkDependencies.cmake
@@ -1,49 +1,25 @@
 #
 # Find dependencies of MoveTk core library
 #
+include(${CMAKE_CURRENT_LIST_DIR}/CreateImportTarget.cmake)
 
 # Prefer config first, then try old module style.
 # TODO: CMake should do this by itself, check version of CMake required for this.
-find_package(Boost CONFIG COMPONENTS iostreams log thread system log_setup graph)
-if(NOT Boost_FOUND)
-    find_package(Boost REQUIRED COMPONENTS iostreams log thread system log_setup graph)
-endif()
 set(Boost_USE_MULTITHREADED 1)
-
-if(NOT TARGET Boost::Boost)
-    if(TARGET Boost::boost)
-        add_library(Boost::Boost ALIAS Boost::boost)
-    else()
-        message(FATAL_ERROR "Cannot find Boost::Boost target" )
-    endif()
-endif()
+find_package(Boost COMPONENTS iostreams log thread system log_setup graph)
+CreateImportTarget(Boost)
 
 find_package(Threads REQUIRED)
 
 # Find GDAL and fix the target if it is not provided.
-find_package(GDAL 3.0 CONFIG)
-if(NOT TARGET GDAL::GDAL)
-    if(NOT DEFINED GDAL_INCLUDE_DIRS OR NOT DEFINED GDAL_LIBRARIES)
-        message(STATUS "Searching for GDAL")
-        find_package(GDAL 3.0 CONFIG)
-        find_package(GDAL 3.0 REQUIRED)
-    endif()
-    if(NOT TARGET GDAL::GDAL)
-        include(${CMAKE_CURRENT_LIST_DIR}/PatchGdalInclude.cmake)
-        PatchGdalInclude()
-        if(NOT TARGET GDAL::GDAL)
-            message(FATAL_ERROR "Could not find and reconstruct GDAL")
-        endif()
-    endif()
-endif()
+find_package(GDAL 3.0 REQUIRED)
+CreateImportTarget(GDAL)
 
 find_package(GeographicLib REQUIRED COMPONENTS SHARED)
+CreateImportTarget(GeographicLib)
 
-find_package(GSL 2.7 REQUIRED NAMES gsl GSL)
-
-find_package(MPFR REQUIRED NAMES mpfr MPFR)
-if(NOT TARGET MPFR::MPFR AND TARGET mpfr::mpfr)
-    add_library(MPFR::MPFR ALIAS mpfr::mpfr)
-endif()
+find_package(GSL 2.5 REQUIRED)
+CreateImportTarget(GSL)
 
 find_package(RapidJSON REQUIRED)
+CreateImportTarget(RapidJSON)
