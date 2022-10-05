c2f470a3ee150cb1ea43c15deed0b388eaa846e5
TUE\bacusters <b.a.custers@tue.nl>
2022-10-05 13:58:56 +0200

    Remove GsTL, not used in the library anymore
    Signed-off-by: TUE\bacusters <b.a.custers@tue.nl>

--- third_party/GsTL/include/GsTL/cdf_estimator.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GTL_MAIN_HEADER_CDF_ESTIMATOR_H__
-#define __GTL_MAIN_HEADER_CDF_ESTIMATOR_H__
-
-#include <GsTL/cdf_estimator/gaussian_cdf_Kestimator.h>
-#include <GsTL/cdf_estimator/gaussian_cdf_coKestimator.h>
-#include <GsTL/cdf_estimator/indicator_cdf_estimator.h>
-
-#endif
--- third_party/GsTL/include/GsTL/sampler.h
+++
@@ -1,6 +0,0 @@
-#ifndef __GTL_MAIN_HEADER_SAMPLER_H__
-#define __GTL_MAIN_HEADER_SAMPLER_H__
-
-#include <GsTL/sampler/random_sampler.h>
-
-#endif
