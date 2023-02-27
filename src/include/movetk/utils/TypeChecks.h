0fc1a7c8ef4dd06d1657d34f5c41d35c5b6d9caf
bac-home\bac <bacusters@gmail.com>
2023-02-28 00:02:25 +0100

    Cleanup some more and remove some unused functionality
    Signed-off-by: bac-home\bac <bacusters@gmail.com>

--- src/include/movetk/utils/TypeChecks.h
+++ src/include/movetk/utils/TypeChecks.h
@@ -29,13 +29,6 @@
 
 #include "movetk/io/ParseDate.h"
 
-#define MOVETK_DEFINE_HAS_TYPE_METAMETHOD(target_type)  \
-	template <typename T, typename = void>                \
-	struct has_##target_type : public std::false_type {}; \
-	template <typename T>                                 \
-	struct has_##target_type<T, std::void_t<typename T::target_type>> : public std::true_type {};
-
-
 /*!
  *
  *  @namespace movetk::utils
@@ -44,124 +37,9 @@
  *	@author Bram Custers (b.a.custers@gmail.com)
  */
 namespace movetk::utils {
-
-/*!@struct is_random_access_iterator
- * @brief A class for checking whether an iterator is
- * a random access iterator
- * @note default value is always set to false
- * @tparam IteratorTag - The iterator category as defined
- * in std::iterator_traits
- */
-template <class IteratorTag>
-struct is_random_access_iterator : public std::is_same<IteratorTag, std::random_access_iterator_tag> {};
-
-/*!@struct is_forward_iterator
- * @brief  A class for checking whether an iterator is
- * a forward iterator
- * @tparam IteratorTag - The iterator category as defined
- * in std::iterator_traits
- */
-template <class IteratorTag>
-struct is_forward_iterator : public std::is_same<IteratorTag, std::forward_iterator_tag> {};
-
-/*!
- * @brief  A class for checking whether an iterator is
- * an output iterator
- * @tparam IteratorTag - The iterator category as defined
- * in std::iterator_traits
- */
-template <class IteratorTag>
-struct is_output_iterator {
-	static constexpr bool value = std::is_same_v<IteratorTag, std::output_iterator_tag>;
-};
-
-/**
- * @brief Predicate to check if the iterator tag corresponds to an input iterator
- * @tparam IteratorTag The iterator tag
-*/
-template <class IteratorTag>
-struct is_input_iterator {
-	static constexpr bool value = std::is_same_v<IteratorTag, std::input_iterator_tag>;
-};
-
 template <size_t dimensions>
 struct is_planar_geometry {
 	static const bool value = dimensions == 2;
-};
-
-/*!@struct  is_NT
- * @brief  A class for checking if the value pointed to
- * by the iterator is of type Kernel::NT
- * @tparam Kernel - A traits class containing collection of expected types
- * @tparam Type - The actual type
- */
-template <class Kernel, class Type>
-struct is_NT : public std::is_same<typename Kernel::NT, std::remove_cv_t<std::remove_reference_t<Type>>> {};
-
-#define MAKE_KERNEL_ELEMENT_CHECK(type_name)                                                      \
-	template <typename Kernel, typename Type>                                                       \
-	struct is_##type_name {                                                                         \
-		static constexpr bool value = std::is_same_v<std::decay_t<Type>, typename Kernel::type_name>; \
-	};
-
-/*!@struct is_Wrapper_Point
- * @brief  A class for checking if the  value pointed
- * to by the iterator is of type \c Kernel::Wrapper_Point
- * @tparam Kernel - A traits class containing collection of expected types
- * @tparam Type - The actual type
- */
-MAKE_KERNEL_ELEMENT_CHECK(Wrapper_Point)
-
-
-/*!@struct is_MovetkPoint
- * @brief A class for checking if the value pointed to
- * by the iterator is of type \c Kernel::MovetkPoint
- * @tparam Kernel - A traits class containing collection of expected types
- * @tparam Type - The actual type
- */
-MAKE_KERNEL_ELEMENT_CHECK(MovetkPoint)
-
-/*!@struct is_MovetkLine
- * @brief A class for checking if the value pointed to
- * by the iterator is of type \c Kernel::MovetkLine
- * @tparam Kernel - A traits class containing collection of expected types
- * @tparam Type - The actual type
- */
-MAKE_KERNEL_ELEMENT_CHECK(MovetkLine)
-
-/*!@struct is_MovetkSegment
- * @brief A class for checking if the value pointed to
- * by the iterator is of type \c Kernel::MovetkSegment
- * @tparam Kernel - A traits class containing collection of expected types
- * @tparam Type - The actual type
- */
-MAKE_KERNEL_ELEMENT_CHECK(MovetkSegment)
-
-/*!@struct is_string
- * @brief A class for checking if the value pointed to
- * by an iterator is of type \c std::string
- * @tparam Type - The actual type
- */
-template <class Type>
-struct is_string : public std::is_same<std::decay_t<Type>, std::string> {};
-
-/*!@struct is_pair
- * @brief A class for checking if the value pointed to
- * by an iterator is of type \c std::pair
- * @tparam Type - The actual type
- */
-template <class Type>
-struct is_pair {
-	static const bool value = false;
-};
-
-/*!
- * @brief A partial specialization of is_pair
- * @details sets value to true iff \c Type == \c std::pair
- */
-template <class T1, class T2>
-struct is_pair<std::pair<T1, T2>> {
-	static const bool value = true;
 };
 
 /**
